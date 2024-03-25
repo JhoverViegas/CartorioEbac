7453f025941a6137e0c35563e9e9811373a329c3
JhoverViegas <131499850+JhoverViegas@users.noreply.github.com>
2024-03-25 14:44:46 -0400

    opção de saída
    Incluso no sistema a opção de saída para o usuario.

--- cartório.c
+++ cartório.c
@@ -128,6 +128,7 @@ int main()
 		printf ("\t1 - Registrar nome\n");
 		printf ("\t2 - Consultar nome\n");
 		printf ("\t3 - Deletar nome\n\n"); 
+		printf("\t4 - Sair do sistema\n\n");
 		printf ("Opção:"); // fim do menu.
 		
 		scanf("%d",&opcao); //armazenano a escolha do usuário.
@@ -148,6 +149,10 @@ int main()
 			deletar();
 			break;     
 			
+			case 4:
+			printf("Obrigado por utilizar o sistema!");	
+			return 0;
+			break;
 			
 			default:
 			printf("Essa opcao não está disponível!\n");
